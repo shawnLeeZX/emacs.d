b8889dcc3c70c49019f122f754d7fd5897f35777
Shuai Li <lishuai918@gmail.com>
2014-09-09 21:06:52 +0800

    Fix an accidental commented code.

--- lisp/init-python-mode.el
+++ lisp/init-python-mode.el
@@ -27,18 +27,18 @@
 ;; (pymacs-load "ropemacs" "rope-")
 ;; (setq ropemacs-enable-autoimport t)
 
-;; ;;; Config for python.el.
-;; (setq
-;;  python-shell-interpreter "ipython"
-;;  python-shell-interpreter-args "--gui=wx --matplotlib=wx --colors=Linux"
-;;  python-shell-prompt-regexp "In \\[[0-9]+\\]: "
-;;  python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
-;;  python-shell-completion-setup-code
-;;    "from IPython.core.completerlib import module_completion"
-;;  python-shell-completion-module-string-code
-;;    "';'.join(module_completion('''%s'''))\n"
-;;  python-shell-completion-string-code
-;;    "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
+;;; Config for python.el.
+(setq
+ python-shell-interpreter "ipython"
+ python-shell-interpreter-args "--gui=wx --matplotlib=wx --colors=Linux"
+ python-shell-prompt-regexp "In \\[[0-9]+\\]: "
+ python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
+ python-shell-completion-setup-code
+   "from IPython.core.completerlib import module_completion"
+ python-shell-completion-module-string-code
+   "';'.join(module_completion('''%s'''))\n"
+ python-shell-completion-string-code
+   "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
 
 
 ;;; Jedi
