0701cd749ad6554c58d9438cab3bb7dbd6406d4e
Shuai Li <lishuai918@gmail.com>
2014-12-15 18:19:57 +0800

    New utility for occur.

--- local-lisp/init/init-local-util.el
+++ local-lisp/init/init-local-util.el
@@ -151,4 +151,26 @@ is chosen from your OS's preference."
     )
   )
 
+;;; Create customized occur to make it only search buffers that has
+;; the same mode of current buffer.
+;; ===================================================================
+(eval-when-compile
+  (require 'cl))
+
+(defun get-buffers-matching-mode (mode)
+  "Returns a list of buffers where their major-mode is equal to MODE"
+  (let ((buffer-mode-matches '()))
+   (dolist (buf (buffer-list))
+     (with-current-buffer buf
+       (if (eq mode major-mode)
+           (add-to-list 'buffer-mode-matches buf))))
+   buffer-mode-matches))
+
+(defun multi-occur-in-this-mode ()
+  "Show all lines matching REGEXP in buffers with this major mode."
+  (interactive)
+  (multi-occur
+   (get-buffers-matching-mode major-mode)
+   (car (occur-read-primary-args))))
+
 (provide `init-local-util)
