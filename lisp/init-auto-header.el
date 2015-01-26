50c48d97947c9c21903ba659964e2c472656f4cf
Shuai Li <lishuai918@gmail.com>
2015-01-26 13:25:03 +0800

    Customized header.

--- lisp/init-auto-header.el
+++ lisp/init-auto-header.el
@@ -4,13 +4,53 @@
 ;; Auto update header when saving.
 (add-hook 'write-file-hooks 'auto-update-file-header)
 
-;; Since I am not writing free software yet, remove it from header.
-(remove-hook 'make-header-hook 'header-free-software)
-
-;; Funtion to insert my contact. Notice that source code of header2.el should be
-;; changed to invoke this function.
+;; Refine related functions.
 (defsubst header-contact ()
   "Insert current my email address."
   (insert header-prefix-string "Contact: lishuai918@gmail.com\n"))
+
+(defsubst header-author (author)
+  "Insert the author name given in the argument"
+  (insert header-prefix-string "Author: " author "\n"))
+
+;; TODO(Shuai) fix the empty line comment.
+;; Redefine header content here.
+(setq make-header-hook '(
+                         ;;header-mode-line
+                         header-title
+                         header-blank
+                         header-file-name
+                         header-description
+                         ;;header-status
+                         (lambda ()
+                           (header-author "Shuai")
+                           )
+                         header-contact
+                         header-maintainer
+                         header-copyright
+                         header-creation-date
+                         ;;header-rcs-id
+                         header-version
+                         header-pkg-requires
+                         ;;header-sccs
+                         header-modification-date
+                         header-modification-author
+                         header-update-count
+                         header-url
+                         header-doc-url
+                         header-keywords
+                         header-compatibility
+                         header-blank
+                         header-lib-requires
+                         header-end-line
+                         header-commentary
+                         header-blank
+                         header-blank
+                         header-blank
+                         header-end-line
+                         header-code
+                         header-eof
+                         )
+      )
 
 (provide 'init-auto-header)
