c4ca616a5759c7baf9a99097e21a89e20ebfe2ca
Shuai Li <lishuai918@gmail.com>
2015-02-11 16:18:26 +0800

    Changed fill width of script to 79 from 80. The change is mainly for accommodating the need for syntax checker for python. 	modified:   local-lisp/init/init-indentation.el

--- local-lisp/init/init-indentation.el
+++ local-lisp/init/init-indentation.el
@@ -6,6 +6,6 @@
 ;; always use spaces, not tabs, when indenting
 (setq indent-tabs-mode nil)
 ; number of characters until the fill column
-(setq-default fill-column 80)
+(setq-default fill-column 79)
 
 (provide 'init-indentation)
