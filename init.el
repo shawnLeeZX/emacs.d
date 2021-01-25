2c382c5a529bbe3080587f44d1856f3761a0ff4e
Shawn <lishuai918@gmail.com>
2021-01-25 15:08:50 +0800

    Upgrade config to use Emacs 27.
    First, package initialization mechanism seems to change in 27,
    so (package-initialize) is disabled in `init.el`
    
    Second, `cl` is deprecated, disable the warning, given that the package
    depending on `cl` may not upgrade.
    
    Third, disable the time calculation in `init-session` given that initialization
    mechanism has changed.

--- init.el
+++ init.el
@@ -7,7 +7,8 @@
 ;; installed packages.  Don't delete this line.  If you don't want it,
 ;; just comment it out by adding a semicolon to the start of the line.
 ;; You may delete these explanatory comments.
-(package-initialize)
+;; Disable since Emacs 27, given it is obsolete.
+;; (package-initialize)
 
 (add-to-list 'load-path (expand-file-name "lisp" user-emacs-directory))
 (require 'init-benchmarking) ;; Measure startup time
--- lisp/init-sessions.el
+++ lisp/init-sessions.el
@@ -2,28 +2,32 @@
 (setq desktop-path (list user-emacs-directory)
       desktop-auto-save-timeout 600)
 (desktop-save-mode 1)
-(defadvice desktop-read (around trace-desktop-errors activate)
-  (let ((debug-on-error t))
-    ad-do-it))
+;; NOTE (Shawn): the following code are disable because since Emacs 27, the
+;; package initialization changes, and I have not taken time to figure out the
+;; new mechanism.
 
-(defadvice desktop-read (around time-restore activate)
-    (let ((start-time (current-time)))
-      (prog1
-          ad-do-it
-        (message "Desktop restored in %.2fms"
-                 (sanityinc/time-subtract-millis (current-time)
-                                                 start-time)))))
+;; (defadvice desktop-read (around trace-desktop-errors activate)
+;;   (let ((debug-on-error t))
+;;     ad-do-it))
 
-(defadvice desktop-create-buffer (around time-create activate)
-  (let ((start-time (current-time))
-        (filename (ad-get-arg 1)))
-    (prog1
-        ad-do-it
-      (message "Desktop: %.2fms to restore %s"
-               (sanityinc/time-subtract-millis (current-time)
-                                               start-time)
-               (when filename
-		 (abbreviate-file-name filename))))))
+;; (defadvice desktop-read (around time-restore activate)
+;;     (let ((start-time (current-time)))
+;;       (prog1
+;;           ad-do-it
+;;         (message "Desktop restored in %.2fms"
+;;                  (sanityinc/time-subtract-millis (current-time)
+;;                                                  start-time)))))
+
+;; (defadvice desktop-create-buffer (around time-create activate)
+;;   (let ((start-time (current-time))
+;;         (filename (ad-get-arg 1)))
+;;     (prog1
+;;         ad-do-it
+;;       (message "Desktop: %.2fms to restore %s"
+;;                (sanityinc/time-subtract-millis (current-time)
+;;                                                start-time)
+;;                (when filename
+;; 		 (abbreviate-file-name filename))))))
 
 ;;----------------------------------------------------------------------------
 ;; Restore histories and registers after saving
